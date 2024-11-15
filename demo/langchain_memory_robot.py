63ee5e6d5f913a5b53fef999d2fe6bb45a04914a
yinchao <ycsuper2819@gmail.com>
2024-11-15 23:24:07 +0800

    init memory demo

--- demo/langchain_memory_robot.py
+++ demo/langchain_memory_robot.py
@@ -1,22 +1,24 @@
-from langchain.memory import ChatMessageHistory
-from langchain.chat_models import ChatOpenAI
+#from langchain.memory import ChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 def main(): 
-    chat = ChatOpenAI(temperature=0)
+    chat = ChatOpenAI(model='gpt-4o', temperature=0)
 
     # 初始化 MessageHistory 对象
     history = ChatMessageHistory()
 
     # 给 MessageHistory 对象添加对话内容
-    history.add_ai_message("你好！")
-    history.add_user_message("中国的首都是哪里？")
+    history.add_ai_message("你好！中国的首都是哪里？")
+    history.add_user_message("有多少个区？")
+    history.add_user_message("最近一年的 gdp 是多少？")
 
     # 执行对话
-    ai_response = chat(history.messages)
+    ai_response = chat.invoke(history.messages)
     print(ai_response)
 
 
